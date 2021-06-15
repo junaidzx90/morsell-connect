28f2b6e44410478d682fbbab8c54955cbc9659a1
junaidzx90 <jsjunu@gmail.com>
2021-06-15 12:12:17 +0600

    1

--- inc/mobilitybuy-admin-view.php
+++ inc/mobilitybuy-admin-view.php
@@ -3,17 +3,17 @@
 <?php
 
 if(isset($_POST['keys_save'])){
-    if(!empty($_POST['parent_site_url'])){
-        $moresell_parent_site_url = esc_url( $_POST['parent_site_url'] );
-        update_option('moresell_parent_site_url', $moresell_parent_site_url);
+    if(isset($_POST['parent_site_url'])){
+        $mobilitybuy_parent_site_url = esc_url( $_POST['parent_site_url'] );
+        update_option('mobilitybuy_parent_site_url', $mobilitybuy_parent_site_url);
     }
-    if(!empty($_POST['consumar_key'])){
+    if(isset($_POST['consumar_key'])){
         $consumar_key = sanitize_key( $_POST['consumar_key'] );
-        update_option('moresell_consumar_key', $consumar_key);
+        update_option('mobilitybuy_consumar_key', $consumar_key);
     }
-    if(!empty($_POST['consumer_secret'])){
+    if(isset($_POST['consumer_secret'])){
         $consumer_secret = sanitize_key( $_POST['consumer_secret'] );
-        update_option('moresell_consumer_secret', $consumer_secret);
+        update_option('mobilitybuy_consumer_secret', $consumer_secret);
     }
 }
 
@@ -25,15 +25,15 @@ if(isset($_POST['keys_save'])){
                 <tbody>
                     <tr>
                         <th><label for="parent_site_url">Parent URL</label></th>
-                        <td><input type="text" name="parent_site_url" id="parent_site_url" value="<?php echo get_option('moresell_parent_site_url','') ?>"></td>
+                        <td><input type="text" name="parent_site_url" id="parent_site_url" value="<?php echo get_option('mobilitybuy_parent_site_url','') ?>"></td>
                     </tr>
                     <tr>
                         <th><label for="consumar_key">Consumer key</label></th>
-                        <td><input type="text" name="consumar_key" id="consumar_key" value="<?php echo get_option('moresell_consumar_key','') ?>"></td>
+                        <td><input type="text" name="consumar_key" id="consumar_key" value="<?php echo get_option('mobilitybuy_consumar_key','') ?>"></td>
                     </tr>
                     <tr>
                         <th><label for="consumer_secret">Consumer secret</label></th>
-                        <td><input type="text" name="consumer_secret" id="consumer_secret" value="<?php echo get_option('moresell_consumer_secret','') ?>"></td>
+                        <td><input type="text" name="consumer_secret" id="consumer_secret" value="<?php echo get_option('mobilitybuy_consumer_secret','') ?>"></td>
                     </tr>
                     <tr>
                         <th>
--- mobilitybuy-connect.php
+++ mobilitybuy-connect.php
@@ -1,85 +1,94 @@
 <?php
 /**
- * Plugin Name: Moresell Connect
- * Plugin Uri: https://github.com/junaidzx90/moresell-connect
+ * Plugin Name: MobilityBuy Connect
+ * Plugin Uri: https://github.com/junaidzx90/mobilitybuy-connect
  * Author: Junayed
  * Author Uri: https://www.fiverr.com/junaidzx90
- * Description: This plugin is a part of the "Moresell" plugin.
+ * Description: This plugin is a part of the "MobilityBuy" plugin.
  * Version: 0.0.1
- * Text Domain:       moresell-connect
+ * Text Domain:       mobilitybuy-connect
  * Domain Path:       /languages
 */
 
-add_action("rest_api_init", "moresell_custom_api_for_inser_product");
-add_action( 'woocommerce_order_status_completed', 'moresell_order_processing', 10, 1);
-add_action("admin_menu", "moresell_connect_menupage");
+add_action("rest_api_init", "mobilitybuy_custom_api_for_inser_product");
+add_action( 'woocommerce_order_status_completed', 'mobilitybuy_order_processing', 10, 1);
+add_action("admin_menu", "mobilitybuy_connect_menupage");
+add_action('woocommerce_product_options_pricing', 'mobilitybuy_add_admin_scripts');
 
 require __DIR__ . '/vendor/autoload.php';
 use Automattic\WooCommerce\Client;
-$url = get_option('moresell_parent_site_url','');
+$url = get_option('mobilitybuy_parent_site_url','');
 if(substr($url , -1)=='/'){
     $url = rtrim($url,"/");
 }
 $_woo = new Client(
     $url, 
-    get_option('moresell_consumar_key',''), 
-    get_option('moresell_consumer_secret',''),
+    get_option('mobilitybuy_consumar_key',''), 
+    get_option('mobilitybuy_consumer_secret',''),
     [
         'version' => 'wc/v3',
     ]
 );
 
 // Menu page
-function moresell_connect_menupage(){
-    add_submenu_page( 'options-general.php', 'Moresell Connect', 'Moresell Connect', 'manage_options', 'moresell-connect', 'moresell_connect_view');
+function mobilitybuy_connect_menupage(){
+    add_submenu_page( 'options-general.php', 'MobilityBuy Connect', 'MobilityBuy Connect', 'manage_options', 'mobilitybuy-connect', 'mobilitybuy_connect_view');
 }
 
 // View page
-function moresell_connect_view(){
-    require_once plugin_dir_path( __FILE__ )."inc/moresell-admin-view.php";
+function mobilitybuy_connect_view(){
+    require_once plugin_dir_path( __FILE__ )."inc/mobilitybuy-admin-view.php";
 }
 
 /**
  * POST REQU API
  */
-function moresell_custom_api_for_inser_product(){
+function mobilitybuy_custom_api_for_inser_product(){
     register_rest_route( 'ms/v1','create',[
         'methods' => 'POST',
-        'callback' => 'moresell_requests_for_product',
+        'callback' => 'mobilitybuy_requests_for_product',
         'permission_callback' => '__return_true'
     ]);
 }
 
+function mobilitybuy_add_admin_scripts( $hook ) {
+    global $post;
+    if(get_post_meta($post->ID,'original_product_id')){
+        ?>
+        <script type="text/javascript">
+        document.getElementById("_regular_price").setAttribute('name', '_price');
+        document.getElementById("_sale_price").setAttribute('name', '_price');
+        document.getElementById("_regular_price").setAttribute('readonly', true);
+        document.getElementById("_sale_price").setAttribute('readonly', true);
+        document.getElementById("_regular_price").setAttribute('disabled', true);
+        document.getElementById("_sale_price").setAttribute('disabled', true);
+        </script>
+        <?php
+    }
+}
+
 /**
- * Attach images to product (feature/ gallery)
+ * Attach images to product (feature)
  */
 function attach_product_thumbnail($post_id, $url, $flag){
-    /*
-    * If allow_url_fopen is enable in php.ini then use this
-    */
+
     $image_url = $url;
     $url_array = explode('/',$url);
     $image_name = $url_array[count($url_array)-1];
-    $image_data = file_get_contents($image_url); // Get image data
+    $image_data = file_get_contents($image_url);
 
-    $upload_dir = wp_upload_dir(); // Set upload folder
-    $unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name ); //    Generate unique name
-    $filename = basename( $unique_file_name ); // Create image file name
+    $upload_dir = wp_upload_dir();
+    $unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name );
+    $filename = basename( $unique_file_name );
 
-    // Check folder permission and define file location
     if( wp_mkdir_p( $upload_dir['path'] ) ) {
         $file = $upload_dir['path'] . '/' . $filename;
     } else {
         $file = $upload_dir['basedir'] . '/' . $filename;
     }
 
-    // Create the image file on the server
     file_put_contents( $file, $image_data );
-
-    // Check image file type
     $wp_filetype = wp_check_filetype( $filename, null );
-
-    // Set attachment data
     $attachment = array(
         'post_mime_type' => $wp_filetype['type'],
         'post_title' => sanitize_file_name( $filename ),
@@ -87,58 +96,112 @@ function attach_product_thumbnail($post_id, $url, $flag){
         'post_status' => 'inherit'
     );
 
-    // Create the attachment
     $attach_id = wp_insert_attachment( $attachment, $file, $post_id );
 
-    // Include image.php
     require_once(ABSPATH . 'wp-admin/includes/image.php');
-
-    // Define attachment metadata
     $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
 
-    // Assign metadata to attachment
     wp_update_attachment_metadata( $attach_id, $attach_data );
 
-    // asign to feature image
+    if( $flag == 1 ){
+        $attach_id_array = get_post_meta($post_id,'_product_image_gallery', true);
+        $imgs = $attach_id_array .= ','.$attach_id;
+        update_post_meta($post_id,'_product_image_gallery',$imgs);
+    }
     if( $flag == 0){
-        // And finally assign featured image to post
         set_post_thumbnail( $post_id, $attach_id );
     }
 
-    // assign to the product gallery
-    if( $flag == 1 ){
-        // Add gallery image to product
-        $attach_id_array = get_post_meta($post_id,'_product_image_gallery', true);
-        $attach_id_array .= ','.$attach_id;
-        update_post_meta($post_id,'_product_image_gallery',$attach_id_array);
-    }
-
+    return true;
 }
 
 /**
  * Category Update
  */
-function moresell_category_update($product_id,$cat){
-    $pid = $product_id;
-    $cat_name = $cat;
+function mobilitybuy_category_update($product_id,$cats){
+    $cats_list = [];
     $taxonomy = 'product_cat';
-    $append = true ;
 
-    $cat  = get_term_by('name', $cat_name , $taxonomy);
-
-    if($cat == false){
-        $cat = wp_insert_term($cat_name, $taxonomy);
-        $cat_id = $cat['term_id'] ;
-    }else{
-        $cat_id = $cat->term_id ;
+    $parents = [];
+    foreach( $cats as $term ) {
+        $parents[$term['term_id']] = $term['name'];
     }
-    //setting post category 
-    $res = wp_set_post_terms($pid,array($cat_id),$taxonomy ,$append);
-    return true;
+
+    $ids = [];
+    foreach( $cats as $term ) {
+        $cats_list[] = $term;
+        $parent = $term['parent'];
+
+        if($parent){
+            $name = '';
+            $haschildid = 0;
+            if(array_key_exists($parent,$parents)){
+                $name = $parents[$parent];
+
+                if(!$haschild = term_exists( $name, $taxonomy )){
+                    $insid = wp_insert_term(
+                        $name, 
+                        $taxonomy
+                    );
+                    $haschildid = $insid['term_id'];
+                    $ids[] = $insid['term_id'];
+                }else{
+                    $chid = wp_update_term( 
+                        $haschild['term_id'], 
+                        $taxonomy
+                    );
+                    $haschildid = $haschild['term_id'];
+                    $ids[] = $haschild['term_id'];
+                }
+            }
+            
+            if($has = term_exists( $term['name'], $taxonomy )){
+                wp_update_term( 
+                    $has['term_id'], 
+                    $taxonomy,
+                    array(
+                        'parent'=> $haschildid,
+                    )
+                );
+                $ids[] = $has['term_id'];
+            }else{
+                $tid = wp_insert_term(
+                    $term['name'], 
+                    $taxonomy,
+                    array(
+                        'parent'=> $haschildid,
+                        'slug' => $term['name']
+                    )
+                );
+                $ids[] = $tid['term_id'];
+            }
+        }else{
+            if($has = term_exists( $term['name'], $taxonomy )){
+                wp_update_term( 
+                    $has['term_id'], 
+                    $taxonomy
+                );
+                $ids[] = $has['term_id'];
+            }else{
+                $tid = wp_insert_term(
+                    $term['name'],
+                    $taxonomy
+                );
+                $ids[] = $tid['term_id'];
+            }
+        }
+        
+    }
+
+    $terms = get_the_terms( $product_id, $taxonomy);
+    foreach($terms as $thisid){
+        wp_remove_object_terms( $product_id, $thisid->name, $taxonomy );
+    }
+    wp_set_post_terms( $product_id, $ids, $taxonomy, true );
 }
 
 // API Callback
-function moresell_requests_for_product($data){
+function mobilitybuy_requests_for_product($data){
     global $wpdb;
     $post_id = 0;
     if($post_id = $wpdb->get_var("SELECT post_id FROM {$wpdb->prefix}postmeta WHERE meta_key = 'original_product_id' AND meta_value = {$data['original_pid']}") ){
@@ -150,6 +213,21 @@ function moresell_requests_for_product($data){
             'post_content' => $data['description'],
             'post_excerpt' => $data['short_description'],
         ));
+
+        $args = array(
+            'post_type'   => 'attachment',
+            'numberposts' => -1,
+            'post_status' => 'any',
+            'post_parent' => $post_id
+        );
+         
+        $attachments = get_posts( $args );
+         
+        if ( $attachments ) {
+            foreach ( $attachments as $attachment ) {
+                wp_delete_attachment( $attachment->ID);
+            }
+        }
     }else{
         $post_id = wp_insert_post(array(
             'post_title' => $data['name'],
@@ -159,12 +237,12 @@ function moresell_requests_for_product($data){
             'post_excerpt' => $data['short_description'],
         ));   
     }
-    
+
     if(!empty($data['the_tags'])){
         wp_set_object_terms($post_id, $data['the_tags'], 'product_tag');
     }
 
-    moresell_category_update($post_id,$data['categories']);
+    mobilitybuy_category_update($post_id,$data['categories']);
     wp_set_object_terms( $post_id, $data['type'], 'product_type' );
     update_post_meta( $post_id, '_visibility', 'visible' );
     update_post_meta( $post_id, '_stock_status', 'instock');
@@ -180,10 +258,10 @@ function moresell_requests_for_product($data){
     update_post_meta( $post_id, '_featured', $data['featured'] );
     update_post_meta( $post_id, '_download_limit', $data['download_limit'] );
     update_post_meta( $post_id, '_download_expiry', $data['download_expiry'] );
-    update_post_meta( $post_id, '_weight', '11' );
-    update_post_meta( $post_id, '_length', '11' );
-    update_post_meta( $post_id, '_width', !empty($data['dimensions']->width )?$data['dimensions']->width:'');
-    update_post_meta( $post_id, '_height', !empty($data['dimensions']->height )?$data['dimensions']->height:'');
+    update_post_meta( $post_id, '_weight', !empty($data['_weight'] )?$data['_weight']:'');
+    update_post_meta( $post_id, '_length', !empty($data['_length'] )?$data['_length']:'');
+    update_post_meta( $post_id, '_width', !empty($data['_width'] )?$data['_width']:'');
+    update_post_meta( $post_id, '_height', !empty($data['_height'] )?$data['_height']:'');
     update_post_meta( $post_id, '_sku', $data['_sku'] );
     update_post_meta( $post_id, '_product_attributes', $data['attributes'] );
     update_post_meta( $post_id, '_sale_price_dates_from', $data['date_on_sale_from'] );
@@ -195,22 +273,21 @@ function moresell_requests_for_product($data){
     update_post_meta( $post_id, '_product_attributes', $data['product_attributes']);
     update_post_meta( $post_id, '_download_limit', $data['download_limit']);
     update_post_meta( $post_id, 'original_product_id', $data['original_pid']);
+
+    // set product is simple/variable/grouped
+    $res = attach_product_thumbnail($post_id,$data['attached'],0);
     
     if(!empty($data['images'])){
         foreach($data['images'] as $image ){
             attach_product_thumbnail($post_id,$image,1);
         }
     }
-
-    // set product is simple/variable/grouped
-    attach_product_thumbnail($post_id,$data['attached'],0);
-
     return 'Success';
 }
 
 // SEND POST REQUEST
 function send_post_to_json($data){
-    $purl = get_option('moresell_parent_site_url','');
+    $purl = get_option('mobilitybuy_parent_site_url','');
     if(substr($purl , -1)=='/'){
         $purl = rtrim($purl,"/");
     }
@@ -232,7 +309,7 @@ function send_post_to_json($data){
 /**
  * Order update to parent site
  */
-function moresell_order_processing($order_id){
+function mobilitybuy_order_processing($order_id){
     global $_woo;
     $order = wc_get_order( $order_id );
     $items = $order->get_items();
